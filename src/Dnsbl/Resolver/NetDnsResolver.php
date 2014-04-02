9c8503b542c81cb28dfddac0c75e835ee31b2bd8
Webeith <webeith@gmail.com>
2014-04-02 19:00:56 +0300

    initial commit

---
+++ src/Dnsbl/Resolver/NetDnsResolver.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the DNSBL package.
+ * (c) Webeith <webeith@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dnsbl\Resolver;
+
+/**
+ * Net DNS resolver
+ *
+ * @author Webeith <webeith@gmail.com>
+ */
+class NetDnsResolver implements InterfaceResolver
+{
+}
