7ea322fc375a07fa59017022750d3256e31fb976
Eugene Buharin <buharin@cupid.com>
2014-04-02 19:00:56 +0300

    initial commit

---
+++ src/Dnsbl/BL/Server.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the DNSBL package.
+ * (c) Webeith <webeith@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dnsbl\BL;
+
+/**
+ * Black list server
+ *
+ * @author Webeith <webeith@gmail.com>
+ */
+class Server
+{
+    protected $host;
+
+}
